c0dba2d6b42ecc2455ee208b7afd309d74fafaeb
Remi <aligastudioopas@gmail.com>
2019-05-23 13:53:30 +0200

    map and filter.

---
+++ filter-map/ex2.js
@@ -0,0 +1,39 @@
+/* Array.prototype.map ET filter - Exercice 2
+
+Il s'agit d'écrire une fonction getCampusesTeachingReact, qui reçoit en argument un tableau
+d'objets décrivant des campus de la Wild Code School. Chaque campus a ces propriétés:
+- city: ville où se trouve le campus
+- curriculums: les cursus proposés dans ce campus (PHP/Symfony, JS/React, JS/Angular, etc.)
+
+En utilisant filter puis map, la fonction doit renvoyer les villes des campus qui proposent
+un cursus JS/React.
+
+Tableau en entrée:
+[
+  { city: 'Bordeaux', curriculums: ['PHP/Symfony', 'JS/React'] },
+  { city: 'La Loupe', curriculums: ['JS/Angular'] },
+  { city: 'Lille', curriculums: ['PHP/Symfony', 'JS/React'] },
+  { city: 'Marseille', curriculums: ['JS/React'] },
+  { city: 'Orléans', curriculums: ['PHP/Symfony'] },
+  { city: 'Reims', curriculums: ['JS/React'] },
+  { city: 'Toulouse', curriculums: ['JEE/Android', 'JS/React'] }
+]
+
+Sortie attendue:
+['Bordeaux', 'Lille', 'Marseille', 'Reims', 'Toulouse']
+
+*/
+
+const getCampusesTeachingReact = campuses => {
+  const filterMap = campuses.filter(campus => {
+    if (campus.curriculums.includes("JS/React")) {
+      return campus;
+    }
+  });
+  const filterMap2 = filterMap.map(campus2 => {
+    return (campus = campus2.city);
+  });
+  return filterMap2;
+};
+
+module.exports = getCampusesTeachingReact;
